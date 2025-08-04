5baf46a227eccd6444587081195ee6caa322b19c
nirmal2604 <nirmalmodi2604@gmail.com>
2025-08-05 02:01:28 +0530

    resolving errors to fecth from render backend

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,11 +1,20 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({ 
+  baseUrl: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  credentials: 'include',  // This tells browser to send cookies
+  prepareHeaders: (headers) => {
+    // Don't need to manually set Authorization header
+    // Browser automatically sends the JWT cookie
+    headers.set('Content-Type', 'application/json');
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ['User'],
-  // it like a prent to other api
-  // it a build in builder
+  // it like a parent to other api
+  // it a built-in builder
   endpoints: (builder) => ({}),
 });
