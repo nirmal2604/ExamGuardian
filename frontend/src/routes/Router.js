f766a53ffdc626b0fa647adeaf899340db4eb14a
nirmal2604 <nirmalmodi2604@gmail.com>
2025-08-05 18:09:09 +0530

    fix router.js

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,31 +1,32 @@
-import React, { lazy } from 'react';
-import { Navigate, useRoutes } from 'react-router-dom';
+import React from 'react';
+import { Navigate, useRoutes, useLocation } from 'react-router-dom';
 import Loadable from '../layouts/full/shared/loadable/Loadable';
 
-const BlankLayout = Loadable(lazy(() => import('../layouts/blank/BlankLayout')));
-const FullLayout = Loadable(lazy(() => import('../layouts/full/FullLayout')));
-const ExamLayout = Loadable(lazy(() => import('../layouts/full/ExamLayout')));
+const BlankLayout = Loadable(React.lazy(() => import('../layouts/blank/BlankLayout')));
+const FullLayout = Loadable(React.lazy(() => import('../layouts/full/FullLayout')));
+const ExamLayout = Loadable(React.lazy(() => import('../layouts/full/ExamLayout')));
 
 // Pages
-const SamplePage = Loadable(lazy(() => import('../views/sample-page/SamplePage')));
-const Success = Loadable(lazy(() => import('../views/Success')));
-const TestPage = Loadable(lazy(() => import('../views/student/TestPage')));
-const ExamPage = Loadable(lazy(() => import('../views/student/ExamPage')));
-const ExamDetails = Loadable(lazy(() => import('../views/student/ExamDetails')));
-const ResultPage = Loadable(lazy(() => import('../views/student/ResultPage')));
-const StudentResultsList = Loadable(lazy(() => import('../views/student/StudentResultsList')));
-const Error = Loadable(lazy(() => import('../views/authentication/Error')));
-const Register = Loadable(lazy(() => import('../views/authentication/Register')));
-const Login = Loadable(lazy(() => import('../views/authentication/Login')));
-const UserAccount = Loadable(lazy(() => import('../views/authentication/UserAccount')));
-const CreateExamPage = Loadable(lazy(() => import('../views/teacher/CreateExamPage')));
-const ExamLogPage = Loadable(lazy(() => import('../views/teacher/ExamLogPage')));
-const AddQuestions = Loadable(lazy(() => import('../views/teacher/AddQuestions')));
-const PrivateRoute = Loadable(lazy(() => import('src/views/authentication/PrivateRoute')));
-const TeacherRoute = Loadable(lazy(() => import('src/views/authentication/TeacherRoute')));
+const SamplePage = Loadable(React.lazy(() => import('../views/sample-page/SamplePage')));
+const Success = Loadable(React.lazy(() => import('../views/Success')));
+const TestPage = Loadable(React.lazy(() => import('../views/student/TestPage')));
+const ExamPage = Loadable(React.lazy(() => import('../views/student/ExamPage')));
+const ExamDetails = Loadable(React.lazy(() => import('../views/student/ExamDetails')));
+const ResultPage = Loadable(React.lazy(() => import('../views/student/ResultPage')));
+const StudentResultsList = Loadable(React.lazy(() => import('../views/student/StudentResultsList')));
+const Error = Loadable(React.lazy(() => import('../views/authentication/Error')));
+const Register = Loadable(React.lazy(() => import('../views/authentication/Register')));
+const Login = Loadable(React.lazy(() => import('../views/authentication/Login')));
+const UserAccount = Loadable(React.lazy(() => import('../views/authentication/UserAccount')));
+const CreateExamPage = Loadable(React.lazy(() => import('../views/teacher/CreateExamPage')));
+const ExamLogPage = Loadable(React.lazy(() => import('../views/teacher/ExamLogPage')));
+const AddQuestions = Loadable(React.lazy(() => import('../views/teacher/AddQuestions')));
+const PrivateRoute = Loadable(React.lazy(() => import('src/views/authentication/PrivateRoute')));
+const TeacherRoute = Loadable(React.lazy(() => import('src/views/authentication/TeacherRoute')));
 
 const Router = () => {
-  return useRoutes([
+  const location = useLocation();
+  const element = useRoutes([
     {
       element: <PrivateRoute />,
       children: [
@@ -76,6 +77,8 @@ const Router = () => {
     },
     { path: '*', element: <Navigate to="/auth/404" /> },
   ]);
+
+  return React.cloneElement(element, { key: location.key });
 };
 
 export default Router;
