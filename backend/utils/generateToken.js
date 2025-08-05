9465797985963a42837f2fca0c4883a4be884fee
nirmal2604 <nirmalmodi2604@gmail.com>
2025-08-05 16:53:25 +0530

    generate token chnage

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "None",
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "None", // This is the key fix
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
